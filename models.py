d64558fae497c9c54ade6f0883688bb98d09af7b
HansenLiu <hansen7799@gmail.com>
2016-12-06 17:39:34 +0800

    initial

---
+++ models.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy_utils import database_exists, create_database
+
+engine = create_engine('mysql://root:rootroot@127.0.0.1/stkb', echo=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+# Set your classes here.
+
+class User(Base):
+    __tablename__ = 'Users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), unique=True)
+    email = Column(String(120), unique=True)
+    password = Column(String(30))
+
+    def __init__(self, name=None, password=None):
+        self.name = name
+        self.password = password
+
+# Create tables.
+def init_db():
+    if not database_exists(engine.url):
+        create_database(engine.url)
+    Base.metadata.create_all(bind=engine)
